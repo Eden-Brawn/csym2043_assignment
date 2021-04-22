609700376c4e81077ebddf0122d52e95f95f4e36
Eden Brawn <edenbrawn@gmail.com>
2021-04-22 15:30:51 +0100

    begun adding javascript to allow user to switch between classes on the classes page

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ function print5() {
 }
 print5();
 
+function class2() {
+    let element = document.getElementById('classcard2');
+    element.style.display = 'block';
+    let preelement = document.getElementById('classcard1');
+    preelement.style.display = 'none';
+}
 
+function switchClass() {
+    let header = document.getElementById('right1');
+    header.addEventListener('click', class2);
 
+    let paragraph = document.getElementById('idp');
+    paragraph.addEventListener('click', updateParagraph);
+}
 
-document.addEventListener('DOMContentLoaded', loadFunction);
+document.addEventListener('DOMContentLoaded', switchClass);
