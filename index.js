a93592885beaf148689e87d220ad089c1c9f2cfd
Eden Brawn <edenbrawn@gmail.com>
2021-04-22 17:45:39 +0100

    made the contact page

--- index.js
+++ index.js
@@ -25,13 +25,36 @@ function classRight() {
     }
 }
 function classLeft() {
-    
+    if (document.getElementById('classcard1').style.display == 'block'){
+        document.getElementById('classcard1').style.display = 'none';
+        document.getElementById('classcard6').style.display = 'block';
+    }
+    else if (document.getElementById('classcard2').style.display == 'block'){
+        document.getElementById('classcard2').style.display = 'none';
+        document.getElementById('classcard1').style.display = 'block';
+    }
+    else if (document.getElementById('classcard3').style.display == 'block'){
+        document.getElementById('classcard3').style.display = 'none';
+        document.getElementById('classcard2').style.display = 'block';
+    }
+    else if (document.getElementById('classcard4').style.display == 'block'){
+        document.getElementById('classcard4').style.display = 'none';
+        document.getElementById('classcard3').style.display = 'block';
+    }
+    else if (document.getElementById('classcard5').style.display == 'block'){
+        document.getElementById('classcard5').style.display = 'none';
+        document.getElementById('classcard4').style.display = 'block';
+    }
+    else if (document.getElementById('classcard6').style.display == 'block'){
+        document.getElementById('classcard6').style.display = 'none';
+        document.getElementById('classcard5').style.display = 'block';
+    }
 }
 function switchClass() {
-    let arrow = document.getElementById('right');
-    arrow.addEventListener('click', classRight);
-    let arrow = document.getElementById('left');
-    arrow.addEventListener('click', classLeft);
+    let right = document.getElementById('right');
+    right.addEventListener('click', classRight);
+    let left = document.getElementById('left');
+    left.addEventListener('click', classLeft);
 }
 
 document.addEventListener('DOMContentLoaded', switchClass);
